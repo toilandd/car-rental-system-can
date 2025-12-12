42cb12ef2304a765f85c27405d1d88b213a3b8fe
toilandd <148343970+toilandd@users.noreply.github.com>
2025-12-12 21:51:31 +0300

    Add Rental class

---
+++ src/main/java/Rental.java
@@ -0,0 +1,16 @@
+public class Rental {
+
+    Customer customer;
+    Car car;
+    int days;
+
+    public Rental(Customer customer, Car car, int days) {
+        this.customer = customer;
+        this.car = car;
+        this.days = days;
+    }
+
+    public double getTotalFee() {
+        return car.calculateRentalFee(days);
+    }
+}
