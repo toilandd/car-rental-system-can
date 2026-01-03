40225ae612c3eb466941deb895269cbde1642781
toilandd <148343970+toilandd@users.noreply.github.com>
2026-01-03 22:12:34 +0300

    Create cartest.java

---
+++ test/cartest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class cartest {
+
+    @Test
+    void testJUnitWorking() {
+        assertEquals(4, 2 + 2);
+    }
+}
