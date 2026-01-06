130d1816169e381d832511efea88125d4a67ffa3
toilandd <148343970+toilandd@users.noreply.github.com>
2026-01-06 19:40:05 +0300

    Add unit test for GasCar rental fee calculation

--- test/cartest.java
+++ test/cartest.java
@@ -7,4 +7,14 @@ class cartest {
     void testJUnitWorking() {
         assertEquals(4, 2 + 2);
     }
+    @Test
+    void testGasCarRentalFee() {
+        Car car = new GasCar(1, "BMW", 100);
+        assertEquals(300, car.calculateRentalFee(3));
+    }
+    
+  
+   
+  
+    
 }
